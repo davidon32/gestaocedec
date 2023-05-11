4f10fbe0c8129670d2a8f2bc6f903259b4f51617
zinhoflag1 <zinhoflag1@gmail.com>
2023-05-11 17:41:43 -0300

    11.05.20231741

--- mod_ajuda/frontEnd/View/ajuda_h/h_pedido_benef/cadastro.php
+++ mod_ajuda/frontEnd/View/ajuda_h/h_pedido_benef/cadastro.php
@@ -13,47 +13,45 @@
 <?php include_once "template/page/corpoHeader.php"; ?>
 
 <?php
-
 $id_pedido = isset($_GET['id_pedido']) ? $_GET['id_pedido'] : "";
 $id = isset($_GET['id']) ? $_GET['id'] : "";
-
 ?>
 
-<legend>Lançamento Beneficiario Prestação de Contas Pedido Nr : <?=$id_pedido;?> </legend>
-<form action="<?=FuncaoBase::geraLink("ajuda", "h_pedido_benef", "gravar");?>" method="post" accept-charset="utf-8" name="frmH_pedido_benef" id="frmH_pedido_benef">
-    
+<legend>Lançamento Beneficiario Prestação de Contas Pedido Nr : <?= $id_pedido; ?> </legend>
+<form action="<?= FuncaoBase::geraLink("ajuda", "h_pedido_benef", "gravar"); ?>" method="post" accept-charset="utf-8" name="frmH_pedido_benef" id="frmH_pedido_benef">
+
     <div class='row'>
-<div class='col-md-6'>
-<label>Nome Beneficiário</label>
-<input type="text" class='form form-control' name='nome_beneficiario' id='nome_beneficiario' maxlength='69' required >
-<input type="hidden" name='id_prest_conta' id='id_prest_conta' required readonly="readonly" value="<?=$id;?>">
-<input type="hidden" name='id_pedido' id='id_pedido' required readonly="readonly" value="<?=$id_pedido;?>">
-</div>
-</div>
-<div class='row'>
-<div class='col-md-2'>
-<label>Identidade do Beneficiário</label>
-<input type="number" class='form form-control' name='rg' id='rg' maxlength='14' required >
-</div>
-</div>
-<div class='row'>
-<div class='col-md-6'>
-<label>Comunidade do Beneficiário</label>
-<input type="text" class='form form-control' name='comunidade' id='comunidade' maxlength='44' required >
-</div>
-</div>
-<div class='row'>
-<div class='col-md-2'>
-<label>Quantidade de Material</label>
-<input type="number" class='form form-control' name='qtd' id='qtd' onblur="this.value = Math.abs(this.value)" max='999' required >
-</div>
-</div>
-<div class='row'>
-<div class='col-md-2'>
-<label>Data de Entrega do Material</label>
-<input type="text" class='form form-control' name='data_entrega' id='data_entrega' maxlength='' required >
-</div>
-</div>
+        <div class='col-md-6'>
+            <label>Nome Beneficiário</label>
+            <input type="text" class='form form-control' name='nome_beneficiario' id='nome_beneficiario' maxlength='69' required >
+            <input type="hidden" name='id_prest_conta' id='id_prest_conta' required readonly="readonly" value="<?= $id; ?>">
+            <input type="hidden" name='id_pedido' id='id_pedido' required readonly="readonly" value="<?= $id_pedido; ?>">
+        </div>
+    </div>
+    <div class='row'>
+        <div class='col-md-2'>
+            <label>Identidade do Beneficiário</label>
+            <input type="number" class='form form-control' name='rg' id='rg' maxlength='14' required >
+        </div>
+    </div>
+    <div class='row'>
+        <div class='col-md-6'>
+            <label>Comunidade do Beneficiário</label>
+            <input type="text" class='form form-control' name='comunidade' id='comunidade' maxlength='44' required >
+        </div>
+    </div>
+    <div class='row'>
+        <div class='col-md-2'>
+            <label>Quantidade de Material</label>
+            <input type="number" class='form form-control' name='qtd' id='qtd' onblur="this.value = Math.abs(this.value)" max='999' required >
+        </div>
+    </div>
+    <div class='row'>
+        <div class='col-md-2'>
+            <label>Data de Entrega do Material</label>
+            <input type="text" class='form form-control' name='data_entrega' id='data_entrega' maxlength='' required>
+        </div>
+    </div>
 
     <div class="col-md-6 text-left">
         <br>
@@ -61,22 +59,18 @@ $id = isset($_GET['id']) ? $_GET['id'] : "";
     </div>
     <div class="col-md-6 text-center">
         <br>
-        <?php
-        
-            if(isset($_GET['id_pedido'])) {
-                print "<a class='btn btn-success' href='".FuncaoBase::geraLink("ajuda", "h_pedido_prest", "index", array('id'=>$_GET['id_pedido']))."'>Voltar</a>";
-            }else {
-                print "<a class='btn btn-success' href='".FuncaoBase::geraLink("ajuda", "h_pedido_benef", "index")."'>Voltar</a>";
-                
-                
-            }
-                
-                ?>
+<?php
+if (isset($_GET['id_pedido'])) {
+    print "<a class='btn btn-success' href='" . FuncaoBase::geraLink("ajuda", "h_pedido_prest", "index", array('id' => $_GET['id_pedido'])) . "'>Voltar</a>";
+} else {
+    print "<a class='btn btn-success' href='" . FuncaoBase::geraLink("ajuda", "h_pedido_benef", "index") . "'>Voltar</a>";
+}
+?>
     </div>
 </form>
-    
-    
-    
+
+
+
 
 <br>
 <!-- =================== RODAPE CORPO ==================== -->
@@ -87,25 +81,16 @@ $id = isset($_GET['id']) ? $_GET['id'] : "";
 <!-- =============== HEADER HTML PAGE ================= -->
 <?php include_once "template/page/rodapePage.php"; ?>
 <script>
-        
+
     $(document).ready(function () {
-    
+
         /* close focus pesquisa */
-       
-    
+
+        $('#data_entrega').mask('99/99/9999');
+
         $("#frmH_pedido_benef").trigger("reset");
-        
-                
-            
-            
-        });
-    
-        
-        
-        
-   
-        
+
 
     });
+
 </script>
-        
