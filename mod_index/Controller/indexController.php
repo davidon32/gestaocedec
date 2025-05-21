a31ea354546a8d3a7d10444240fa2af8c3aca2fe
Felipe Carneiro <flipecar97@gmail.com>
2025-05-21 11:03:03 -0300

    Atualizar lógica de redirecionamento no método logar para suportar diferentes ambientes

--- mod_index/Controller/indexController.php
+++ mod_index/Controller/indexController.php
@@ -29,7 +29,13 @@ class indexController extends Controller
         # producao
         //header('Location: https://www.sdc.mg.gov.br/index.php');
         #develop
-        header('Location: http://sdc.net:8081/login');
+        if (($_SERVER['HTTP_HOST'] == 'sistema.defesacivil.mg.gov.br') || ($_SERVER['HTTP_HOST'] == 'www.sistema.defesacivil.mg.gov.br')) {
+            header('Location: https://www.sdc.mg.gov.br/index.php');
+        } else if ($_SERVER['HTTP_HOST'] == 'sdcold.net:8081') {
+            header('Location: http://sdc.net:8081/login');
+        } else {
+            header('Location: http://'.getenv('SDC_HOST').'/login');
+        }
 
     }
 
