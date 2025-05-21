7b71e7a78b52b593dd0c092a6ff5add64e5f66a4
defesacivil <gestaocedecmg@gmail.com>
2025-05-21 11:53:13 -0300

    .

--- mod_index/backEnd/View/index/index2.php
+++ mod_index/backEnd/View/index/index2.php
@@ -11,29 +11,28 @@
 <?php include_once "template/page/corpoHeader.php"; ?>
 
 <style>
-.dashboard-button {
-    background-color: white;
-    border: 0;
-    border-radius: 12px;
-    box-shadow: 0 0 8px rgba(0, 0, 0, 0.1);
-    text-align: left;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    cursor: pointer;
-    padding: 10%;
-    transition: transform 0.2s ease, box-shadow 0.2s ease;
-}
+    .dashboard-button {
+        background-color: white;
+        border: 0;
+        border-radius: 12px;
+        box-shadow: 0 0 8px rgba(0, 0, 0, 0.1);
+        text-align: left;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        cursor: pointer;
+        padding: 10%;
+        transition: transform 0.2s ease, box-shadow 0.2s ease;
+    }
 
-.dashboard-button:hover{
-    transform: translateY(-1px);
-    box-shadow: 0 0px 16px rgba(0, 0, 0, 0.2);
-}
+    .dashboard-button:hover {
+        transform: translateY(-1px);
+        box-shadow: 0 0px 16px rgba(0, 0, 0, 0.2);
+    }
 
-.dashboard-button i {
-    padding-bottom: 10px;
-}
-
+    .dashboard-button i {
+        padding-bottom: 10px;
+    }
 </style>
 
 <div class="col-md-12 text-center">
@@ -56,53 +55,58 @@
     ?>
 
 
-    <?php if($_COOKIE['seguranca']['idUser'] == 1) { ?>
-    <div class="col-md-3 text-center" style="height: 200px;">
-        <a class="thumbnail dashboard-button" href="?token=<?= hash("sha256", md5(VERSAO) . date('dmY')) . "&ac=itn&modulo=index&controller=index&action=rat" ?>" title="Módulo RAT">
-            <i class="fa fa-file fa-5x"></i>RAT
-        </a>
-    </div>
-    <div class="col-md-3 text-center" style="height: 200px;">
-        <a class="thumbnail dashboard-button" href="?token=<?= hash("sha256", md5(VERSAO) . date('dmY')) . "&ac=itn&modulo=index&controller=index&action=vistoria" ?>" title="Relatório de Vistoria/Interdição">
-            <i class="fa fa-clipboard fa-5x"></i>Vistoria/Interdição
-        </a>
-    </div>
-    <div class="col-md-3 text-center" style="height: 200px;">
-        <a class="thumbnail dashboard-button" href="?token=<?= hash("sha256", md5(VERSAO) . date('dmY')) . "&ac=itn&modulo=index&controller=index&action=paebm" ?>" title="Protocolo PaeBM">
-            <i class="fa fa-list-alt fa-5x"></i>PAE
-        </a>
-    </div>
-    <?php if ($_COOKIE['seguranca']['idUser'] == 1) { ?>
-    <div class="col-md-3 text-center" style="height: 200px;">
-        <a class="thumbnail dashboard-button" href="?token=<?= hash("sha256", md5(VERSAO) . date('dmY')) . "&ac=itn&modulo=index&controller=index&action=compdec" ?>" title="Informações do Compdec">
-            <i class="fa fa-users fa-5x"></i>Informações COMPDEC (CADASTRO)
-        </a>
-    </div>
-    <?php } ?>
-    <?php
-    # autorização entrada sistema cisterna
+        <div class="col-md-3 text-center" style="height: 200px;">
+            <a class="thumbnail dashboard-button" href="?token=<?= hash("sha256", md5(VERSAO) . date('dmY')) . "&ac=itn&modulo=index&controller=index&action=rat" ?>" title="Módulo RAT">
+                <i class="fa fa-file fa-5x"></i>RAT
+            </a>
+        </div>
+        <div class="col-md-3 text-center" style="height: 200px;">
+            <a class="thumbnail dashboard-button" href="?token=<?= hash("sha256", md5(VERSAO) . date('dmY')) . "&ac=itn&modulo=index&controller=index&action=vistoria" ?>" title="Relatório de Vistoria/Interdição">
+                <i class="fa fa-clipboard fa-5x"></i>Vistoria/Interdição
+            </a>
+        </div>
+        <div class="col-md-3 text-center" style="height: 200px;">
+            <a class="thumbnail dashboard-button" href="?token=<?= hash("sha256", md5(VERSAO) . date('dmY')) . "&ac=itn&modulo=index&controller=index&action=paebm" ?>" title="Protocolo PaeBM">
+                <i class="fa fa-list-alt fa-5x"></i>PAE
+            </a>
+        </div>
+        <?php if ($_COOKIE['seguranca']['idUser'] == 1) { ?>
+            <div class="col-md-3 text-center" style="height: 200px;">
+                <a class="thumbnail dashboard-button" href="?token=<?= hash("sha256", md5(VERSAO) . date('dmY')) . "&ac=itn&modulo=index&controller=index&action=compdec" ?>" title="Informações do Compdec">
+                    <i class="fa fa-users fa-5x"></i>Informações COMPDEC (CADASTRO)
+                </a>
+            </div>
+        <?php
+        # autorização entrada sistema cisterna
         $autorizacao = [
             "M1296844",
-            "S126282",
-            "M1552631",
+            "S126068",
+            "X3611403",
+            "S106904",
+            "M350254",
+            "S104069",
+            "S100460",
+            "M1489596",
+            "S165427"
         ];
-        if( (in_array($_COOKIE['seguranca']['login'], $autorizacao)) || ($_COOKIE['seguranca']['idUser'] == 855)  ){
+        if ((in_array($_COOKIE['seguranca']['login'], $autorizacao)) || ($_COOKIE['seguranca']['idUser'] == 855)) {
             //<!--CISTERNA -->
-    ?>
-        <div class="col-md-3 text-center" style="height: 200px;">
-            <a class="thumbnail dashboard-button" href="?token=<?= hash("sha256", md5(VERSAO) . date('dmY')) . "&ac=itn&modulo=index&controller=index&action=cisterna" ?>" title="Projeto Convivência com a Seca">
-                <i class="fa fa-tint fa-5x"></i>Projeto Convivência com a Seca
-            </a></div>
-    <?php
+        ?>
+            <div class="col-md-3 text-center" style="height: 200px;">
+                <a class="thumbnail dashboard-button" href="?token=<?= hash("sha256", md5(VERSAO) . date('dmY')) . "&ac=itn&modulo=index&controller=index&action=cisterna" ?>" title="Projeto Convivência com a Seca">
+                    <i class="fa fa-tint fa-5x"></i>Projeto Convivência com a Seca
+                </a>
+            </div>
+        <?php
         } else {
             print "<div class=\"col-md-3 text-center\" style=\"height: 200px;\">";
             print "<img class=\"imgCinza\" src=\"core/imagem/poco.png\" width=\"105\" title=\"Usuario sem Acesso !\">";
             print " <br>Projeto Convivência com a Seca";
             print "</div>";
         }
-    ?>
+        ?>
 </div>
-<?php } ?>
+
 <!--RAT DESABILITAR -->
 <!--<div class="col-md-3 text-center" style="height: 190px; ">
             <a class="thumbnail" title="Relatório de Atividades"><img width="155" src="core/imagem/rat_conversao.png"><br />RAT</a>
